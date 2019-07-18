fb29ce0c91d767f5aebc931b3f966cfe2863cb70
Cecilia <ceciliabelen.b@gmail.com>
2019-07-17 21:58:23 -0300

    Agrego última clase de MVC, el código de Shippers está fallando. Revisar.

---
+++ SolucionMvcABM/mvcABM/Controllers/EnvioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvcABM.Models;
+
+namespace mvcABM.Controllers
+{
+    public class EnvioController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Envio
+        public ActionResult Index()
+        {
+            return View(db.Shippers.ToList());
+        }
+
+        // GET: Envio/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipper shipper = db.Shippers.Find(id);
+            if (shipper == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipper);
+        }
+
+        // GET: Envio/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Envio/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipperID,CompanyName,Phone")] Shipper shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shipper);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shipper);
+        }
+
+        // GET: Envio/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipper shipper = db.Shippers.Find(id);
+            if (shipper == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipper);
+        }
+
+        // POST: Envio/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShipperID,CompanyName,Phone")] Shipper shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipper).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        // GET: Envio/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipper shipper = db.Shippers.Find(id);
+            if (shipper == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipper);
+        }
+
+        // POST: Envio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shipper shipper = db.Shippers.Find(id);
+            db.Shippers.Remove(shipper);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SolucionMvcABM/mvcABM/Controllers/ShipperController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcABM.Controllers
+{
+    public class ShipperController : Controller
+    {
+        Models.NorthwindEntities db = new Models.NorthwindEntities();
+        // GET: Shipper
+        public ActionResult Index()
+        {
+            var lista = (from s in db.Shippers select s).ToList();
+            return View(lista);
+        }
+        //Método de acción "Create GET"
+        [HttpGet] //Parte del modo de compartir con el usuario
+        public ActionResult Create()
+        {
+            Models.Shipper shipper = new Models.Shipper();
+            shipper.Phone = "000";
+            return View(shipper);
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Shipper shipper)
+        {
+            db.Shippers.Add(shipper);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
